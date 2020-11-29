84ddffe9b5bf87a5d0f0e85c62427b6ff0bafb3c
Jaren Goldberg <fyrlex@gmail.com>
2020-11-29 02:33:54 -0500

    new api and stats

--- commands/minecraft/mcserver.js
+++ commands/minecraft/mcserver.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const e = require("express");
 const mc = require('minecraft-server-util');
 module.exports = {
   aliases: ["mcstatus", "status"],
@@ -8,17 +9,17 @@ module.exports = {
   name: "mcserver",
   toggleable: true,
   run: async (bot, message, args, prefix, guildData) => {
-    let language = bot.utils.getLanguage(bot, guildData.language)
+    let language = bot.utils.getLanguage(bot, guildData.language);
     if (message.member.hasPermission("MANAGE_GUILD") && args[0]) {
       if (args[0] === "remove") {
-        bot.db.prepare("UPDATE guilddata SET mcserverip=? WHERE guildid=?").run("none", message.guild.id)
-        bot.mcservers.delete(message.guild.id)
+        bot.db.prepare("UPDATE guilddata SET mcserverip=? WHERE guildid=?").run("none", message.guild.id);
+        bot.mcservers.delete(message.guild.id);
         let embed = new Discord.MessageEmbed()
           .setColor(bot.colors.green)
           .setDescription(bot.translate(bot, language, "mcserver.removed")
             .replace(/{CHECK}/g, bot.emoji.check)
             .replace(/{INFO}/g, bot.emoji.info));
-        return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e) });
+        return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
       }
       let updating = new Discord.MessageEmbed()
         .setColor(bot.colors.main)
@@ -28,54 +29,44 @@ module.exports = {
       try {
         updatingMessage = await message.channel.send(updating);
       } catch (e) {
-        bot.error(bot, message, language, e);
+        return bot.error(bot, message, language, e);
       }
-      mc.queryFull(args[0]).then(server => {
-        bot.db.prepare("UPDATE guilddata SET mcserverip=? WHERE guildid=?").run(args[0], message.guild.id)
-        bot.mcservers.set(message.guild.id, args[0])
-        updating.setColor(bot.colors.green)
+      let tryip = await bot.fetch(`https://api.mcsrvstat.us/2/${args[0]}`).then(res => res.json()).then(json => {
+        return json.ip;
+      })
+      if (tryip.length >= 1) {
+        bot.db.prepare("UPDATE guilddata SET mcserverip=? WHERE guildid=?").run(args[0], message.guild.id);
+        bot.mcservers.set(message.guild.id, args[0]);
+        updating.setColor(bot.colors.green);
         updating.setDescription(bot.translate(bot, language, "mcserver.updated").join("\n")
           .replace(/{CHECK}/g, bot.emoji.check)
           .replace(/{SERVER}/g, args[0])
           .replace(/{INFO}/g, bot.emoji.info)
-          .replace(/{PREFIX}/g, prefix))
-        updatingMessage.edit(updating).catch(e => bot.error(bot, message, language, e));
-      }).catch(e => {
-        mc.status(args[0]).then(server => {
-            bot.db.prepare("UPDATE guilddata SET mcserverip=? WHERE guildid=?").run(args[0], message.guild.id)
-            bot.mcservers.set(message.guild.id, args[0])
-            updating.setColor(bot.colors.green)
-            updating.setDescription(bot.translate(bot, language, "mcserver.updated").join("\n")
-              .replace(/{CHECK}/g, bot.emoji.check)
-              .replace(/{SERVER}/g, args[0])
-              .replace(/{INFO}/g, bot.emoji.info)
-              .replace(/{PREFIX}/g, prefix))
-            updatingMessage.edit(updating).catch(e => bot.error(bot, message, language, e));
-          })
-          .catch(e => {
-            updating.setColor(bot.colors.red);
-            updating.setFooter(bot.translate(bot, language, "mcserver.errorfooter"));
-            updating.setDescription(bot.translate(bot, language, "mcserver.error").join("\n")
-              .replace(/{CROSS}/g, bot.emoji.cross)
-              .replace(/{SERVER}/g, args[0]));
-            updatingMessage.edit(updating).catch(e => {
-              bot.error(bot, message, language, e);
-            })
-          });
-      })
+          .replace(/{PREFIX}/g, prefix));
+        updatingMessage.edit(updating).catch(e => { return bot.error(bot, message, language, e); });
+      } else {
+        updating.setColor(bot.colors.red);
+        updating.setFooter(bot.translate(bot, language, "mcserver.errorfooter"));
+        updating.setDescription(bot.translate(bot, language, "mcserver.error").join("\n")
+          .replace(/{CROSS}/g, bot.emoji.cross)
+          .replace(/{SERVER}/g, args[0]));
+        updatingMessage.edit(updating).catch(e => { return bot.error(bot, message, language, e); });
+      }
     } else {
       if (bot.mcservers.has(message.guild.id)) {
         let server = bot.mcservers.get(message.guild.id)
-        mc.queryFull(server).then(data => {
+        bot.fetch(`https://api.mcsrvstat.us/2/${server}`).then(res => res.json()).then(data => {
+          let players = data.players.list ? data.players.list.map(p => `**-** ${p.replace(/_/g, "\_")}`).join("\n") : `**-** *${bot.translate(bot, language, "none")}*`
           let embed = new Discord.MessageEmbed()
-            .setColor(bot.colors.main)
-            .setDescription([
-              `${bot.emoji.check} **Status:** \`online\``,
-              `📡 **IP:** \`${server}\``,
-              ``,
-              `👥 **Players:** ${data.onlinePlayers}/${data.maxPlayers}`,
-              `${data.players ? data.players.map(p => `**-** ${p.replace(/_/g, "\_")}`).join("\n") : `**-** *${bot.translate(bot, language, "none")}*`}`])
-          return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e) });
+            .setColor(data.online ? bot.colors.main : bot.colors.red)
+            .setDescription(bot.translate(bot, language, "mcserver.status").join("\n")
+              .replace(/{CHECK}/g, bot.emoji.check)
+              .replace(/{STATUS}/g, data.online)
+              .replace(/{SERVER}/g, server)
+              .replace(/{ONLINEPLAYERS}/g, data.players.online)
+              .replace(/{MAXPLAYERS}/g, data.players.max)
+              .replace(/{PLAYERS}/g, players));
+          return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
         }).catch(e => {
           let embed = new Discord.MessageEmbed()
             .setColor(bot.colors.red)
@@ -83,7 +74,7 @@ module.exports = {
             .setDescription(bot.translate(bot, language, "mcserver.error").join("\n")
               .replace(/{CROSS}/g, bot.emoji.cross)
               .replace(/{SERVER}/g, server));
-          return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e) });
+          return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
         });
       } else {
         let embed = new Discord.MessageEmbed()
@@ -91,8 +82,8 @@ module.exports = {
           .setDescription(bot.translate(bot, language, "mcserver.noip").join("\n")
             .replace(/{CROSS}/g, bot.emoji.cross)
             .replace(/{INFO}/g, bot.emoji.info)
-            .replace(/{PREFIX}/g, prefix))
-        return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e) });
+            .replace(/{PREFIX}/g, prefix));
+        return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
       }
     }
   }
