4ca87d42f0351bba2efa49537825ccfd4bb3f90b
Jaren Goldberg <fyrlex@gmail.com>
2020-11-29 12:57:49 -0500

    formatted code

--- commands/minecraft/mcachievement.js
+++ commands/minecraft/mcachievement.js
@@ -8,34 +8,33 @@ module.exports = {
   name: "mcachievement",
   toggleable: true,
   run: async (bot, message, args, prefix, guildData) => {
-    // message.delete({timeout:500}).catch(e=>{});
     let language = bot.utils.getLanguage(bot, guildData.language);
     let title;
     let subtitle;
     if (args[0]) {
       let parts = args.join(" ").replace(/(\\|\/)/g, " ").trim().split("|");
-      let ptitle = parts[0] ? parts[0].trim() : parts[0]
-      let psubtitle = parts[1] ? parts[1].trim() : parts[1]
+      let ptitle = parts[0] ? parts[0].trim() : parts[0];
+      let psubtitle = parts[1] ? parts[1].trim() : parts[1];
       if (ptitle && ptitle.length >= 1) {
         title = ptitle;
       } else {
-        title = bot.translate(bot, language, "mcachievement.defaulttitle")
+        title = bot.translate(bot, language, "mcachievement.defaulttitle");
       }
       title = title.split(" ").join("%20")
       if (psubtitle && psubtitle.length >= 1) {
         subtitle = psubtitle;
       } else {
-        subtitle = bot.translate(bot, language, "mcachievement.defaultsubtitle")
+        subtitle = bot.translate(bot, language, "mcachievement.defaultsubtitle");
       }
-      subtitle = subtitle.split(" ").join("%20")
+      subtitle = subtitle.split(" ").join("%20");
     } else {
       let embed = new Discord.MessageEmbed()
         .setColor(bot.colors.red)
+        .setImage("https://i.imgur.com/xQPByzh.png")
         .setDescription(bot.translate(bot, language, "mcachievement.usage").join("\n")
           .replace(/{CROSS}/g, bot.emoji.cross)
-          .replace(/{PREFIX}/g, prefix))
-        .setImage("https://i.imgur.com/xQPByzh.png")
-      return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e) });
+          .replace(/{PREFIX}/g, prefix));
+      return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
     }
     let item = Math.floor(Math.random() * 150) + 1
     let finallink = link.replace(/{ITEM}/g, item)
@@ -43,10 +42,10 @@ module.exports = {
       .replace(/{SUBTITLE}/g, subtitle);
     let embed = new Discord.MessageEmbed()
       .setColor(bot.colors.lightblue)
+      .setImage(finallink)
       .setDescription(bot.translate(bot, language, "mcachievement.success").join("\n")
         .replace(/{CHECK}/g, bot.emoji.check)
-        .replace(/{USER}/g, message.author))
-      .setImage(finallink)
-    return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e) });
+        .replace(/{USER}/g, message.author));
+    return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
   }
 }
