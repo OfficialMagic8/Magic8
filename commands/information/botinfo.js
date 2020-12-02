559cb79e986cbc4429cb3a2063665a979fdd462d
Jaren Goldberg <fyrlex@gmail.com>
2020-12-02 18:36:14 -0500

    fixed info menus

--- commands/information/botinfo.js
+++ commands/information/botinfo.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js")
+const { MessageEmbed } = require("discord.js")
 module.exports = {
   aliases: ["bi", "binfo"],
   category: "INFO",
@@ -8,9 +8,9 @@ module.exports = {
   run: async (bot, message, args, prefix, guildData) => {
     let language = bot.utils.getLanguage(bot, guildData.language);
     let uptime = bot.ms(bot.uptime);
-    let memory = getMemoryUsage();
-    let maxRam = 2048;
-    console.log(`~ RAM: ${memory}/${maxRam} MB (${Math.round((memory * 100) / maxRam)}%)`);
+    // let memory = getMemoryUsage();
+    // let maxRam = 2048;
+    // console.log(`~ RAM: ${memory}/${maxRam} MB (${Math.round((memory * 100) / maxRam)}%)`);
     let embed = new MessageEmbed()
       .setAuthor(bot.translate(bot, language, "botinfo.title").replace(/{BOTNAME}/g, bot.user.username))
       .setColor(bot.colors.main)
@@ -32,11 +32,12 @@ module.exports = {
         .replace(/{CREATIONDATE}/g, bot.user.createdAt.toLocaleString().split("GMT")[0].trim())
         .replace(/{DONATELINK}/g, bot.config.donatelink)
         .replace(/{SUPPORTSERVER}/g, bot.invite)
-        .replace(/{GITHUB}/g, bot.github.languages))
+        .replace(/{GITHUB}/g, bot.github.languages)
+        .replace(/{STATUS}/g, bot.config.uptime))
     return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e) });
-    function getMemoryUsage() {
-      let total_rss = bot.fs.readFileSync("/sys/fs/cgroup/memory/memory.stat", "utf8").split("\n").filter(l => l.startsWith("total_rss"))[0].split(" ")[1];
-      return Math.round(Number(total_rss) / 1e6) - 60;
-    }
+    // function getMemoryUsage() {
+    //   let total_rss = bot.fs.readFileSync("/sys/fs/cgroup/memory/memory.stat", "utf8").split("\n").filter(l => l.startsWith("total_rss"))[0].split(" ")[1];
+    //   return Math.round(Number(total_rss) / 1e6) - 60;
+    // }
   }
 }
