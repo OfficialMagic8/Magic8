443e567460207fa505fd9ab6b84409c9f4ba12dd
Jaren Goldberg <fyrlex@gmail.com>
2020-12-03 17:00:31 -0500

    menus for setting mcserver

--- commands/minecraft/mcserver.js
+++ commands/minecraft/mcserver.js
@@ -10,6 +10,16 @@ module.exports = {
   run: async (bot, message, args, prefix, guildData) => {
     let language = bot.utils.getLanguage(bot, guildData.language);
     if (message.member.hasPermission("MANAGE_GUILD")) {
+      if (!args[0] && args[0].toLowerCase() === "help") {
+        let embed = new MessageEmbed()
+          .setColor(bot.colors.main)
+          .setAuthor(bot.translate(bot, language, "mcserver.menutitle")
+            .replace(/{BOTNAME}/g, bot.user.username))
+          .setDescription(bot.translate(bot, language, "mcserver.helpmenu").join("\n")
+            .replace(/{PREFIX}/g, prefix)
+            .replace(/{INFO}/g, bot.emoji.info))
+        return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+      }
       if (args[0] && args[0].toLowerCase() === "remove") {
         bot.db.prepare("UPDATE guilddata SET mcserverip=? WHERE guildid=?").run("none", message.guild.id);
         bot.mcservers.delete(message.guild.id);
@@ -21,6 +31,16 @@ module.exports = {
         return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
       }
       if (args[0] && args[0].toLowerCase() === "set") {
+        if (!args[1]) {
+          let embed = new MessageEmbed()
+            .setColor(bot.colors.main)
+            .setAuthor(bot.translate(bot, language, "mcserver.menutitle")
+              .replace(/{BOTNAME}/g, bot.user.username))
+            .setDescription(bot.translate(bot, language, "mcserver.setmenu").join("\n")
+              .replace(/{INFO}/g, bot.emoji.info)
+              .replace(/{PREFIX}/g, prefix));
+          return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e,); })
+        }
         let updating = new MessageEmbed()
           .setColor(bot.colors.main)
           .setDescription(bot.translate(bot, language, "mcserver.checking")
@@ -66,23 +86,73 @@ module.exports = {
         } catch (e) { }
         bot.fetch(`https://api.mcsrvstat.us/2/${server}`).then(res => res.json()).then(data => {
           let getplayers = data.players.list ? data.players.list : false;
-          let selectedplayers = getPlayers(bot, message, args, getplayers)
-          let embed = new MessageEmbed()
-            .setColor(data.online ? bot.colors.main : bot.colors.red)
-            .setImage(`http://status.mclive.eu/${server}/${server}/banner.png`)
-            .setDescription(bot.translate(bot, language, "mcserver.status").join("\n")
-              .replace(/{CHECK}/g, bot.emoji.check)
-              .replace(/{STATUS}/g, data.online ? bot.translate(bot, language, "mcserver.online") : bot.translate(bot, language, "mcserver.offline"))
-              .replace(/{SERVER}/g, server)
-              .replace(/{VERSIONS}/g, data.version ? data.version : bot.translate(bot, language, "none"))
-              .replace(/{ONLINEPLAYERS}/g, data.players.online ? data.players.online : "0")
-              .replace(/{MAXPLAYERS}/g, data.players.max ? data.players.max : "0")
-              .replace(/{PLAYERS}/g, getplayers.length >= 1 ? selectedplayers.join("\n") : ""));
-          if (thumbnail) embed.setThumbnail(thumbnail);
-          if (getplayers) embed.setFooter(bot.translate(bot, language, "mcserver.pagefooter")
-            .replace(/{PAGE}/g, page)
-            .replace(/{TOTALPAGES}/g, totalpages));
-          return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+          if (getplayers) {
+            let mapped = getplayers.map(p => `**•** ${p.replace(/_/g, "\_")}`);
+            let size = mapped.length;
+            let math = size / 8;
+            let fullpagecount = Math.floor(math);
+            let totalpages;
+            if (!Number.isInteger(math)) {
+              totalpages = fullpagecount + 1;
+            } else {
+              totalpages = fullpagecount;
+            }
+            let page = args[0] ? Math.abs(Math.floor(parseInt(args[0]))) : 1;
+            if (isNaN(page) || page > totalpages || page < 1) {
+              let embed = new MessageEmbed()
+                .setColor(bot.colors.red)
+                .setDescription(bot.translate(bot, language, "mcserver.invalidpage").join("\n")
+                  .replace(/{CROSS}/g, bot.emoji.cross)
+                  .replace(/{INPUT}/g, page)
+                  .replace(/{INFO}/g, bot.emoji.info)
+                  .replace(/{TOTALPAGES}/g, totalpages));
+              return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+            }
+            let lastitemindex = page * 8;
+            let selectedplayers = [];
+            for (map of mapped) {
+              if (page === totalpages) {
+                if (mapped.indexOf(map) + 1 <= size && mapped.indexOf(map) + 1 > fullpagecount * 8) {
+                  selectedplayers.push(map);
+                }
+              }
+              if (mapped.indexOf(map) + 1 <= lastitemindex && mapped.indexOf(map) + 1 > lastitemindex - 8) {
+                if (!selectedplayers.includes(map)) selectedplayers.push(map);
+              }
+            }
+            let embed = new MessageEmbed()
+              .setColor(data.online ? bot.colors.main : bot.colors.red)
+              .setImage(`http://status.mclive.eu/${server}/${server}/banner.png`)
+              .setDescription(bot.translate(bot, language, "mcserver.status").join("\n")
+                .replace(/{CHECK}/g, bot.emoji.check)
+                .replace(/{STATUS}/g, data.online ? bot.translate(bot, language, "mcserver.online") : bot.translate(bot, language, "mcserver.offline"))
+                .replace(/{SERVER}/g, server)
+                .replace(/{VERSIONS}/g, data.version ? data.version : bot.translate(bot, language, "none"))
+                .replace(/{ONLINEPLAYERS}/g, data.players.online ? data.players.online : "0")
+                .replace(/{MAXPLAYERS}/g, data.players.max ? data.players.max : "0")
+                .replace(/{PLAYERS}/g, getplayers ? selectedplayers.join("\n") : "")
+                .replace(/{INFO}/g, bot.emoji.info))
+              .setFooter(bot.translate(bot, language, "mcserver.pagefooter")
+                .replace(/{PAGE}/g, page)
+                .replace(/{TOTALPAGES}/g, totalpages));
+            if (thumbnail) embed.setThumbnail(thumbnail);
+            return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+          } else {
+            let embed = new MessageEmbed()
+              .setColor(data.online ? bot.colors.main : bot.colors.red)
+              .setImage(`http://status.mclive.eu/${server}/${server}/banner.png`)
+              .setDescription(bot.translate(bot, language, "mcserver.status").join("\n")
+                .replace(/{CHECK}/g, bot.emoji.check)
+                .replace(/{STATUS}/g, data.online ? bot.translate(bot, language, "mcserver.online") : bot.translate(bot, language, "mcserver.offline"))
+                .replace(/{SERVER}/g, server)
+                .replace(/{VERSIONS}/g, data.version ? data.version : bot.translate(bot, language, "none"))
+                .replace(/{ONLINEPLAYERS}/g, data.players.online ? data.players.online : "0")
+                .replace(/{MAXPLAYERS}/g, data.players.max ? data.players.max : "0")
+                .replace(/{PLAYERS}/g, "")
+                .replace(/{INFO}/g, bot.emoji.info));
+            if (thumbnail) embed.setThumbnail(thumbnail);
+            return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+          }
         }).catch(e => {
           console.error(e)
           let embed = new MessageEmbed()
@@ -101,44 +171,6 @@ module.exports = {
             .replace(/{INFO}/g, bot.emoji.info)
             .replace(/{PREFIX}/g, prefix));
         return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
-      }
-    }
-    function getPlayers(bot, message, args, getplayers) {
-      if (getplayers) {
-        let mapped = getplayers.map(p => `**•** ${p.replace(/_/g, "\_")}`);
-        let size = mapped.length;
-        let math = size / 8;
-        let fullpagecount = Math.floor(math);
-        let totalpages;
-        if (!Number.isInteger(math)) {
-          totalpages = fullpagecount + 1;
-        } else {
-          totalpages = fullpagecount;
-        }
-        let page = args[0] ? Math.abs(Math.floor(parseInt(args[0]))) : 1;
-        if (isNaN(page) || page > totalpages || page < 1) {
-          let embed = new MessageEmbed()
-            .setColor(bot.colors.red)
-            .setDescription(bot.translate(bot, language, "mcserver.invalidpage").join("\n")
-              .replace(/{CROSS}/g, bot.emoji.cross)
-              .replace(/{INPUT}/g, page)
-              .replace(/{INFO}/g, bot.emoji.info)
-              .replace(/{TOTALPAGES}/g, totalpages));
-          return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
-        }
-        let lastitemindex = page * 8;
-        let selectedplayers = [];
-        for (map of mapped) {
-          if (page === totalpages) {
-            if (mapped.indexOf(map) + 1 <= size && mapped.indexOf(map) + 1 > fullpagecount * 5) {
-              selectedplayers.push(map);
-            }
-          }
-          if (mapped.indexOf(map) + 1 <= lastitemindex && mapped.indexOf(map) + 1 > lastitemindex - 5) {
-            if (!selectedplayers.includes(map)) selectedplayers.push(map);
-          }
-        }
-        return selectedplayers;
       }
     }
   }
