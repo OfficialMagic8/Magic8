85e6755173ee7173f3be114e1734a9154c5cf5fc
Jaren Goldberg <fyrlex@gmail.com>
2020-12-02 19:04:09 -0500

    popular - error catching

--- commands/information/popular.js
+++ commands/information/popular.js
@@ -7,25 +7,29 @@ module.exports = {
   name: "popular",
   run: async (bot, message, args, prefix, guildData) => {
     let language = bot.utils.getLanguage(bot, guildData.language);
-    let popularcommands = await bot.fetch("https://statcord.com/logan/stats/484148705507934208").then(res => res.json())
-      .then(json => {
-        return json.popular;
-      }).catch(e => { return bot.error(bot, message, language, e); });
-    let popularcommandsarray = [];
-    for (command of popularcommands) {
-      let cmd = bot.commands.get(command.name)
-      if (popularcommands.indexOf(command) < 10) {
-        popularcommandsarray.push(`${cmd.emoji} \`${cmd.name}\` (${parseInt(command.count).toLocaleString("en")})`);
-      }
-    }
     let embed = new MessageEmbed()
-      .setAuthor(bot.translate(bot, language, "popular.title")
-        .replace(/{BOTNAME}/g, bot.user.username))
       .setColor(bot.colors.main)
-      .setFooter(bot.translate(bot, language, "popular.footer"))
-      .setThumbnail(bot.user.displayAvatarURL({ format: "png" }))
-      .setDescription(bot.translate(bot, language, "popular.description").join("\n")
+      .setDescription(bot.translate(bot, language, "popular.loading")
+        .replace(/{LOADING}/g, bot.emoji.loading)
+        .replace(/{USER}/g, message.author));
+    let embedmessage = await message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+    bot.fetch("https://statcord.com/logan/stats/484148705507934208").then(res => res.json()).then(json => {
+      let popularcommands = json.popular;
+      let popularcommandsarray = [];
+      for (command of popularcommands) {
+        let cmd = bot.commands.get(command.name);
+        if (popularcommands.indexOf(command) < 10) {
+          popularcommandsarray.push(`${cmd.emoji} \`${cmd.name}\` (${parseInt(command.count).toLocaleString("en")})`);
+        }
+      }
+      embed.setAuthor(bot.translate(bot, language, "popular.title")
+        .replace(/{BOTNAME}/g, bot.user.username))
+      embed.setColor(bot.colors.main)
+      embed.setFooter(bot.translate(bot, language, "popular.footer"))
+      embed.setThumbnail(bot.user.displayAvatarURL({ format: "png" }))
+      embed.setDescription(bot.translate(bot, language, "popular.description").join("\n")
         .replace(/{COMMANDS}/g, popularcommandsarray.join("\n")));
-    return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+      return embedmessage.edit(embed).catch(e => { return bot.error(bot, message, language, e); });
+    }).catch(e => { return bot.error(bot, message, language, e); })
   }
 }  
