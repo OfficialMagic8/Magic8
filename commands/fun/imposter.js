9df467d4f8880d44d9c2ed5cf4290f7b8e214bd4
Jaren Goldberg <fyrlex@gmail.com>
2020-12-04 20:51:13 -0500

    imposter command

---
+++ commands/fun/imposter.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require("discord.js");
+const colors = ["blue", "red", "darkgreen", "lime", "orange", "pink", "white", "yellow", "brown", "purple", "black", "cyan"];
+const isimposter = ["true", "false"];
+const baseURL = "https://vacefron.nl/api/ejected?name={USERNAME}&impostor={ISIMPOSTER}&crewmate={COLOR}";
+module.exports = {
+  aliases: [],
+  category: "FUN",
+  description: "Discover the real imposter in your Discord server",
+  emoji: "🕵️",
+  name: "imposter",
+  toggleable: true,
+  run: async (bot, message, args, prefix, guildData) => {
+    let language = bot.utils.getLanguage(bot, guildData.language);
+    let color = colors[Math.floor(Math.random() * colors.length)];
+    let imposter = isimposter[Math.floor(Math.random() * isimposter.length)];
+    let url = baseURL.replace(/{USERNAME}/g, message.author.username).replace(/{ISIMPOSTER}/g, imposter).replace(/{COLOR}/g, color);
+    if (!args[0]) {
+      let embed = new MessageEmbed()
+        .setColor(bot.colors.main)
+        .setAuthor(bot.translate(bot, language, "imposter.title")
+          .replace(/{USERNAME}/g, message.author.username), message.author.displayAvatarURL({ format: "png" }))
+        .setImage(url);
+      return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+    }
+  }
+}  
