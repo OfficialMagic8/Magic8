d79c4db3819c68db7b252290909dc17cb5d0c333
Jaren Goldberg <fyrlex@gmail.com>
2020-12-04 20:52:22 -0500

    dev fixes

--- commands/private/reload.js
+++ commands/private/reload.js
@@ -7,11 +7,22 @@ module.exports = {
   name: "reload",
   dev: true,
   run: async (bot, message, args, prefix, guildData) => {
-    bot.utils.loadCommands(bot);
-    bot.utils.loadEvents(bot);
-    let embed = new MessageEmbed()
-      .setColor(bot.colors.green)
-      .setDescription(`${bot.emoji.check} **Commands & Events were attempted to reload!**`);
-    return message.channel.send(embed).catch(e => { });
+    try {
+      bot.utils.loadCommands(bot);
+      bot.utils.loadEvents(bot);
+      let embed = new MessageEmbed()
+        .setColor(bot.colors.green)
+        .setDescription(`${bot.emoji.check} **Commands & Events were attempted to reload!**`);
+      return message.channel.send(embed).catch(e => { });
+    } catch (e) {
+      let embed = new MessageEmbed()
+        .setColor(bot.colors.red)
+        .setDescription([
+          `${bot.emoji.cross} **Error**`,
+          `\`\`\`xl`,
+          `${e}`,
+          `\`\`\``])
+      return message.channel.send(embed).catch(e => { })
+    }
   }
 }  
