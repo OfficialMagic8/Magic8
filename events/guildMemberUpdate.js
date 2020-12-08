46669a48b929ad8345ecc3e8330c34e62930a941
Jaren Goldberg <fyrlex@gmail.com>
2020-12-08 15:52:02 -0500

    fix lfg if role manually removed

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -5,7 +5,7 @@ module.exports = {
     let guild = bot.guilds.cache.get((oldMember || newMember).guild.id);
     if (bot.lfgroles.has(guild.id) && guild.roles.cache.has(bot.lfgroles.get(guild.id)) && !newMember.roles.cache.has(bot.lfgroles.get(guild.id)) && oldMember.roles.cache.has(bot.lfgroles.get(guild.id))) {
       let users = bot.lfgusers.get(guild.id);
-      if (users.includes(newMember.id)) {
+      if (users && users.includes(newMember.id)) {
         let removed = users.filter(user => user !== newMember.id);
         bot.lfgusers.set(guild.id, removed);
         if (bot.lfgusers.get(guild.id).length <= 0) {
