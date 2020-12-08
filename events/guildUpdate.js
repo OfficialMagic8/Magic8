a9603162980c118f469193b50fed7d29f1c0dc32
Jaren Goldberg <fyrlex@gmail.com>
2020-12-08 15:51:39 -0500

    fix for renaming guild in database

--- events/guildUpdate.js
+++ events/guildUpdate.js
@@ -1,9 +1,10 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "guildUpdate",
-  run: async (bot, newGuild, oldGuild) => {
+  run: async (bot, oldGuild, newGuild) => {
     if (newGuild.name !== oldGuild.name) {
       bot.db.prepare("UPDATE guilddata SET guildname=? WHERE guildid=?").run(newGuild.name, newGuild.id);
+      bot.udb.prepare("UPDATE usagedata SET guildname=? WHERE guildid=?").run(newGuild.name, newGuild.id);
     }
   }
 }
