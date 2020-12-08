f1d4db0ce1181c8ee1c72075c086f511976103cf
Jaren Goldberg <fyrlex@gmail.com>
2020-12-08 15:48:35 -0500

    christmas - error catching

--- commands/fun/christmas.js
+++ commands/fun/christmas.js
@@ -8,15 +8,14 @@ module.exports = {
   toggleable: true,
   run: async (bot, message, args, prefix, guildData) => {
     let language = bot.utils.getLanguage(bot, guildData.language)
-    let data = await bot.fetch("https://christmas-days.anvil.app/_/api/get_days").then(res => res.json())
+    bot.fetch("https://christmas-days.anvil.app/_/api/get_days").then(res => res.json())
       .then(json => {
-        return json;
+        let embed = new MessageEmbed()
+          .setColor(bot.colors.green)
+          .setThumbnail("https://i.imgur.com/ACaHu30.png")
+          .setDescription(bot.translate(bot, language, "christmas.description").join("\n")
+            .replace(/{DAYS}/g, json["Days to Christmas"]))
+        return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e) });
       }).catch(e => { return bot.error(bot, message, language, e); });
-    let embed = new MessageEmbed()
-      .setColor(bot.colors.green)
-      .setThumbnail("https://i.imgur.com/ACaHu30.png")
-      .setDescription(bot.translate(bot, language, "christmas.description").join("\n")
-        .replace(/{DAYS}/g, data["Days to Christmas"]))
-    return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e) });
   }
 }  
