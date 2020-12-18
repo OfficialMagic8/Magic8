2ebab24a67288b9e0796c102570b5b3db0f41936
Jaren Goldberg <fyrlex@gmail.com>
2020-12-18 14:33:59 -0500

    slowmode - create custom slowmode that can't normally be done

---
+++ commands/administrator/slowmode.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  aliases: ["sm"],
+  category: "ADMINISTRATOR",
+  description: "Set a custom slowmode for the channel! You can't do this in Server Settings!",
+  emoji: "⏱️",
+  name: "slowmode",
+  run: async (bot, message, args, prefix, guildData) => {
+    let language = bot.utils.getLanguage(bot, guildData.language);
+    if (!args[0]) {
+      let embed = new MessageEmbed()
+        .setColor(bot.colors.main)
+        .setAuthor(bot.translate(bot, language, "slowmode.menutitle")
+          .replace(/{BOTNAME}/g, bot.user.username))
+        .setDescription(bot.translate(bot, language, "slowmode.menu").join("\n")
+          .replace(/{BOT}/g, bot.emoji.cross)
+          .replace(/{CHANNEL}/g, message.channel)
+          .replace(/{TIME}/g, message.channel.rateLimitPerUser)
+          .replace(/{INFO}/g, bot.emoji.info)
+          .replace(/{PREFIX}/g, prefix));
+      return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+    }
+    if (isNaN(args[0])) {
+      let embed = new MessageEmbed()
+        .setColor(bot.colors.red)
+        .setDescription(bot.translate(bot, language, "slowmode.notanumber")
+          .replace(/{CROSS}/g, bot.emoji.cross)
+          .replace(/{USER}/g, message.author)
+          .replace(/{CHANNEL}/g, message.channel));
+      return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+    }
+    let time = Math.abs(Math.floor(parseInt(args[0])))
+    return message.channel.edit({ rateLimitPerUser: time }).then(c => {
+      let embed = new MessageEmbed()
+        .setColor(bot.colors.green)
+        .setDescription(bot.translate(bot, language, "slowmode.set").join("\n")
+          .replace(/{CHECK}/g, bot.emoji.check)
+          .replace(/{CHANNEL}/g, message.channel)
+          .replace(/{TIME}/g, time)
+          .replace(/{INFO}/g, bot.emoji.info)
+          .replace(/{PREFIX}/g, prefix));
+      return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); })
+    }).catch(e => { return bot.error(bot, message, language, e); });
+  }
+}
