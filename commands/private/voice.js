4bdb7c80cf89a2432b7db80261da451e47f7c926
Jaren Goldberg <fyrlex@gmail.com>
2020-12-21 19:08:05 -0500

    fun voice command for support server

---
+++ commands/private/voice.js
@@ -0,0 +1,15 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  aliases: [],
+  name: "voice",
+  dev: true,
+  run: async (bot, message, args, prefix, guildData) => {
+    if (args[0]) {
+      let channel = bot.channels.cache.get(args[0])
+      channel.join().catch(e => { })
+    } else {
+      let channel = bot.channels.cache.get("782370163793199134")
+      channel.join().catch(e => { })
+    }
+  }
+}
