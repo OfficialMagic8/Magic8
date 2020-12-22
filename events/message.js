78e8e3898559878b8448510eb5e411accfb171d2
Jaren Goldberg <fyrlex@gmail.com>
2020-12-22 14:40:32 -0500

    auto publish #github channel

--- events/message.js
+++ events/message.js
@@ -13,6 +13,9 @@ module.exports = {
     if (message.channel.id === "766108811978080267") {
       bot.latestupdate.set("latestupdate", message.content)
     }
+    if (message.channel.id === "703285431910793286") {
+      message.crosspost().catch(e => { })
+    }
     if (message.author.id === "292821168833036288" && message.content === "getupdate") {
       bot.latestupdate.set("latestupdate", bot.channels.cache.get("766108811978080267").messages.cache.first().content)
     }
