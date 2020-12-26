0a283535bc47430bc125830898734703d356d3e1
Jaren Goldberg <fyrlex@gmail.com>
2020-12-26 00:20:37 -0500

    dev tools

--- commands/private/addcommand.js
+++ commands/private/addcommand.js
@@ -6,15 +6,15 @@ module.exports = {
   run: async (bot, message, args, prefix, guildData) => {
     let guildsids = bot.guilds.cache.keyArray();
     let loaded = bot.udb.prepare("SELECT * FROM usagedata").all().filter(row => guildsids.includes(row.guildid));
-    loaded.forEach(row => {
-      let usage = JSON.parse(row.usage);
-      let o = {
-        command: "randomnumber",
-        category: "UTILITIES",
-        usage: 0
-      }
-      usage.push(o);
-      bot.udb.prepare("UPDATE usagedata SET usage=? WHERE guildid=?").run(JSON.stringify(usage), row.guildid);
-    });
+    // loaded.forEach(row => {
+    //   let usage = JSON.parse(row.usage);
+    //   let o = {
+    //     command: "randomnumber",
+    //     category: "UTILITIES",
+    //     usage: 0
+    //   }
+    //   usage.push(o);
+    //   bot.udb.prepare("UPDATE usagedata SET usage=? WHERE guildid=?").run(JSON.stringify(usage), row.guildid);
+    // });
   }
 }
--- commands/private/sortcommands.js
+++ commands/private/sortcommands.js
@@ -4,33 +4,40 @@ module.exports = {
   name: "sortcommands",
   dev: true,
   run: async (bot, message, args, prefix, guildData) => {
-    let guildsids = bot.guilds.cache.keyArray();
-    let loaded = bot.udb.prepare("SELECT * FROM usagedata").all().filter(row => guildsids.includes(row.guildid));
-    loaded.forEach(row => {
-      let usage = JSON.parse(row.usage);
-      usage.sort(function (a, b) {
-        let nameA = a.command.toUpperCase();
-        let nameB = b.command.toUpperCase();
-        if (nameA < nameB) {
-          return -1;
-        }
-        if (nameA > nameB) {
-          return 1;
-        }
-        return 0;
+    try {
+      let guildsids = bot.guilds.cache.keyArray();
+      let loaded = bot.udb.prepare("SELECT * FROM usagedata").all().filter(row => guildsids.includes(row.guildid));
+      loaded.forEach(row => {
+        let usage = JSON.parse(row.usage);
+        usage.sort(function (a, b) {
+          let nameA = a.command.toUpperCase();
+          let nameB = b.command.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+        usage.sort(function (a, b) {
+          let nameA = a.category.toUpperCase();
+          let nameB = b.category.toUpperCase();
+          if (!a.category || !b.category) {
+            console.log(`${row.guildid} ${b.category} ${b.commanf}`)
+          }
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+        bot.udb.prepare("UPDATE usagedata SET usage=? WHERE guildid=?").run(JSON.stringify(usage), row.guildid);
       });
-      usage.sort(function (a, b) {
-        let nameA = a.category.toUpperCase();
-        let nameB = b.category.toUpperCase();
-        if (nameA < nameB) {
-          return -1;
-        }
-        if (nameA > nameB) {
-          return 1;
-        }
-        return 0;
-      });
-      bot.udb.prepare("UPDATE usagedata SET usage=? WHERE guildid=?").run(JSON.stringify(usage), row.guildid);
-    });
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
