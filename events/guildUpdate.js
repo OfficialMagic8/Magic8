a32319fe1c2633aac1f6e71eca5cb1f281173083
Jaren Goldberg <fyrlex@gmail.com>
2021-01-16 14:02:25 -0500

    guild vote database check

--- events/guildUpdate.js
+++ events/guildUpdate.js
@@ -5,6 +5,7 @@ module.exports = {
     if (newGuild.name !== oldGuild.name) {
       bot.db.prepare("UPDATE guilddata SET guildname=? WHERE guildid=?").run(newGuild.name, newGuild.id);
       bot.udb.prepare("UPDATE usagedata SET guildname=? WHERE guildid=?").run(newGuild.name, newGuild.id);
+      bot.vdb.prepare("UPDATE votedata SET guildname=? WHERE guildid=?").run(newGuild.name, newGuild.id);
     }
   }
 }
