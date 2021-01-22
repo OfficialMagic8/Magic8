1ba546c8f711beb8df58be75d67ce624d04d2ae3
Jaren Goldberg <fyrlex@gmail.com>
2021-01-22 16:27:00 -0500

    can now @ other users

--- commands/entertainment/imposter.js
+++ commands/entertainment/imposter.js
@@ -13,14 +13,28 @@ module.exports = {
     let language = bot.utils.getLanguage(bot, guildData.language);
     let color = colors[Math.floor(Math.random() * colors.length)];
     let imposter = isimposter[Math.floor(Math.random() * isimposter.length)];
-    let url = baseURL.replace(/{USERNAME}/g, message.author.username).replace(/{ISIMPOSTER}/g, imposter).replace(/{COLOR}/g, color);
+    let target = message.author;
+    if (args[0]) {
+      try {
+        let id = args[0].replace(/[^0-9]/g, "");
+        target = bot.users.cache.get(id) || await bot.users.fetch(id);
+      } catch (e) {
+        let embed = new MessageEmbed()
+          .setColor(bot.colors.red)
+          .setDescription(bot.translate(bot, language, "it")
+            .replace(/{CROSS}/g, bot.emoji.cross)
+            .replace(/{USER}/g, message.author));
+        return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+      }
+    }
+    let url = baseURL.replace(/{USERNAME}/g, target.username.split(" ").join("%20")).replace(/{ISIMPOSTER}/g, imposter).replace(/{COLOR}/g, color);
     if (!args[0]) {
       let embed = new MessageEmbed()
         .setColor(bot.colors.main)
         .setImage(url)
         .setAuthor(bot.translate(bot, language, "imposter.title")
-          .replace(/{USERNAME}/g, message.author.username)
-          .replace(/{GUILDNAME}/g, message.guild.name), message.author.displayAvatarURL({ format: "png" }));
+          .replace(/{USERNAME}/g, target.username)
+          .replace(/{GUILDNAME}/g, message.guild.name), target.displayAvatarURL({ format: "png" }));
       return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
     }
   }
