9aed5e8838a5cab29708cbd88d711c4e231e3a39
Fyrlex <fyrlex@gmail.com>
2021-02-07 14:23:21 -0500

    can now view specific ads

--- commands/information/partners.js
+++ commands/information/partners.js
@@ -12,16 +12,44 @@ module.exports = {
   emoji: "🤝",
   name: "partners",
   run: async (bot, message, args, prefix, guildData) => {
+    let validids = [];
+    bot.ads.forEach(ad => {
+      if (ad.id) validids.push(ad.id);
+    });
     let language = bot.utils.getLanguage(bot, guildData.language);
-    let embed = new MessageEmbed()
-      .setColor(bot.colors.main)
-      .setAuthor(`${bot.user.username} Partners & Affiliates`)
-      .setThumbnail(bot.user.displayAvatarURL({ format: "png" }))
-      .setDescription(partners.join("\n")
-        .replace(/{BREMEA}/g, bot.emoji.bremea)
-        .replace(/{PLEXUSMC}/g, bot.emoji.plexusmc)
-        .replace(/{SHOPERY}/g, bot.emoji.shopery)
-        .replace(/{SOMETHINGHOST}/g, bot.emoji.somethinghost));
-    return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+    if (!args[0]) {
+      let embed = new MessageEmbed()
+        .setColor(bot.colors.main)
+        .setAuthor(`${bot.user.username} Partners & Affiliates`)
+        .setThumbnail(bot.user.displayAvatarURL({ format: "png" }))
+        .setDescription(partners.join("\n")
+          .replace(/{BREMEA}/g, bot.emoji.bremea)
+          .replace(/{PLEXUSMC}/g, bot.emoji.plexusmc)
+          .replace(/{SHOPERY}/g, bot.emoji.shopery)
+          .replace(/{SOMETHINGHOST}/g, bot.emoji.somethinghost));
+      return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+    } else {
+      if (!validids.includes(args[0].toLowerCase())) {
+        let embed = new MessageEmbed()
+          .setColor(bot.colors.red)
+          .setDescription(bot.translate(bot, language, "partners.invalid").join("\n")
+            .replace(/{CROSS}/g, bot.emoji.cross)
+            .replace(/{INPUT}/g, args[0])
+            .replace(/{INFO}/g, bot.emoji.info));
+        return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+      } else {
+        let ad = bot.ads.find(ad => ad.id === args[0].toLowerCase());
+        let embed = new MessageEmbed()
+          .setColor(bot.colors.main)
+          .setTitle(`${ad.name} - ${ad.type}`)
+          .setFooter(`Want you to be an partner or affiliate? Contact Fyrlex#2740`)
+          .setDescription(ad.description.join("\n")
+            .replace(/{INFO}/g, bot.emoji.info)
+            .replace(/{ADSINFO}/g, bot.docs.ads));
+        if (ad.image) embed.setImage(ad.image);
+        if (ad.thumbnail) embed.setThumbnail(ad.thumbnail);
+        return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+      }
+    }
   }
 }
