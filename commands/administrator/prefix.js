58efd3a9c69af89616c656c8aafe7000bfb2c956
Jaren Goldberg <fyrlex@gmail.com>
2021-02-13 23:14:39 -0500

    multilingual support

--- commands/administrator/prefix.js
+++ commands/administrator/prefix.js
@@ -12,12 +12,18 @@ module.exports = {
         .setColor(bot.colors.main)
         .setAuthor(`${bot.user.username} - Prefix Settings`)
         .setThumbnail(bot.user.displayAvatarURL({ formant: "png" }))
-        .setDescription([
-          `**Current Prefix:** \`${prefix}\``,
-          ``,
-          `${bot.emoji.info} To update the prefix, type: \`${prefix}prefix <prefix>\``,
-          ``,
-          `${bot.emoji.warning} Make sure you don't use special characters like chinese or unicode emojis!`]);
+        .setDescription(bot.translate(bot, language, "prefix.menu").join("\n")
+          .replace(/{PREFIX}/g, prefix)
+          .replace(/{INFO}/g, bot.emoji.info)
+          .replace(/{WARNING}/g, bot.emoji.warning));
+      return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
+    }
+    if (![`<@${bot.user.id}>`, `<@!${bot.user.id}>`].includes(args[0]) && (message.mentions.roles.size > 0 || message.mentions.users.size > 0)) {
+      let embed = new MessageEmbed()
+        .setColor(bot.colors.red)
+        .setDescription(bot.translate(bot, language, "prefix.hasmention")
+          .replace(/{CROSS}/g, bot.emoji.cross)
+          .replace(/{USER}/g, message.author));
       return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
     }
     let newPrefix = args[0];
@@ -25,10 +31,11 @@ module.exports = {
     bot.db.prepare("UPDATE guilddata SET prefix=? WHERE guildid=?").run(newPrefix, message.guild.id);
     let embed = new MessageEmbed()
       .setColor(bot.colors.main)
-      .setDescription([
-        `${bot.emoji.check} **Updated Prefix:** \`${newPrefix}\``,
-        ``,
-        `${bot.emoji.info} ${bot.user} will always be an active prefix in case your forget yours.`]);
+      .setDescription(bot.translate(bot, language, "prefix.updated").join("\n")
+        .replace(/{CHECK}/g, bot.emoji.check)
+        .replace(/{NEWPREFIX}/g, newPrefix)
+        .replace(/{INFO}/g, bot.emoji.info)
+        .replace(/{BOT}/g, bot.user));
     return message.channel.send(embed).catch(e => { return bot.error(bot, message, language, e); });
   }
 }
